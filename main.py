b42e17e292d5f79249ba3eee5ea9322bfdc639da
Fluidize <mbxiao1@gmail.com>
2023-08-17 18:59:20 -0500

    e

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import requests
 from progress_bar import *
 
-file_url = "https://www.mathworks.com/content/dam/mathworks/mathworks-dot-com/moler/random.pdf"
+file_url = ""
 
 #request data
 r = requests.get(file_url, stream=True)
 total_size_in_bytes= int(r.headers.get('content-length', 0))
 block_size = 1024
+file_name = file_url.split('/')[-1] #last url directory 
+
+
+
+
+
+
+
+
 progress_bar = progress_bar_manual(total_size_in_bytes)
-
-
-
-with open("file.pdf", "wb") as file:
+with open(file_name, "wb") as file:
 
 	for chunk in r.iter_content(block_size):
 		if chunk:
 			file.write(chunk)
 			progress_bar.update(len(chunk))
-
 progress_bar.close()
 			
