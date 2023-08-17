84da3e9699163c413ccfe36c69c4d77888e2b50b
Fluidize <mbxiao1@gmail.com>
2023-08-17 18:48:29 -0500

    a

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from progress_bar import *
 
 file_url = "https://www.mathworks.com/content/dam/mathworks/mathworks-dot-com/moler/random.pdf"
 
+#request data
 r = requests.get(file_url, stream=True)
 total_size_in_bytes= int(r.headers.get('content-length', 0))
 block_size = 1024
 progress_bar = progress_bar_manual(total_size_in_bytes)
+
+
 
 with open("file.pdf", "wb") as file:
 
