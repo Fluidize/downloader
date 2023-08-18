9820df8dca5b5cba1c9b9ec243f2a495bbc5f4ee
Fluidize <mbxiao1@gmail.com>
2023-08-17 21:12:19 -0500

    eee

--- main.py
+++ main.py
@@ -8,28 +8,30 @@ print(displaytext("1. Download from URL\n2. Settings\n3. EXIT", custom_border_le
 
 
 
+while True:
+	option = input('')
+	if option == '1':
+		file_url = input('URL: ')
 
-file_url = input('URL: ')
-
-#request data
-r = requests.get(file_url, stream=True)
-total_size_in_bytes= int(r.headers.get('content-length', 0))
-block_size = 1024
-file_name = file_url.split('/')[-1] #last url directory 
-
-
-
-
+		#request data
+		r = requests.get(file_url, stream=True)
+		total_size_in_bytes= int(r.headers.get('content-length', 0))
+		block_size = 1024
+		file_name = file_url.split('/')[-1] #last url directory 
 
 
 
 
-progress_bar = progress_bar_manual(total_size_in_bytes)
-with open(file_name, "wb") as file:
 
-	for chunk in r.iter_content(block_size):
-		if chunk:
-			file.write(chunk)
-			progress_bar.update(len(chunk))
-progress_bar.close()
+
+
+
+		progress_bar = progress_bar_manual(total_size_in_bytes)
+		with open(file_name, "wb") as file:
+
+			for chunk in r.iter_content(block_size):
+				if chunk:
+					file.write(chunk)
+					progress_bar.update(len(chunk))
+		progress_bar.close()
 			
